0462c386e09aa151c4c2dd8c415702fb9859ef73
jogi96 <joris.gisler@stud.hslu.ch>
2025-10-29 13:08:39 +0100

    wrote prediction notebook and startet with fitting

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BASE_DIR = Path(__file__).parent
 TEST_PIC_DIR = BASE_DIR / "Data/Testdata/pictures"
 TEST_FILE_DIR = BASE_DIR / "DATA/Testdata/Files"
 YOLO_MODEL_DIR = BASE_DIR / "YOLO_Test_Model/best.pt"
-TRAIN_DIR = BASE_DIR / "Data/Train"
+TRAIN_DIR = BASE_DIR / "Data/train"
