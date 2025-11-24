7c7116437be26765d108dea7de657a192b47275c
jogi96 <joris.gisler@stud.hslu.ch>
2025-11-24 12:00:33 +0100

    made changes to validation

--- Pipeline/training_Instance_Segmentation.py
+++ Pipeline/training_Instance_Segmentation.py
@@ -1,8 +1,19 @@
+#Setting Working directory
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path.cwd().parent))
+
+#Import the config file so that only the Filename needs to be changed in the _read_segy function
+from config import *
+
 from ultralytics import YOLO
 
-
+#load pretrained model
 model = YOLO("yolo11n-seg.pt")
-results = model.train(data=r"C:\vscode\PAINDGPR\Annotated_Data\YOLO_Segmentation_Hyperbola\data.yaml",epochs = 200,batch=4 ,project="YOLO_Model_seg", name="Hyperbola_seg")
+
+#train on custom dataset
+results = model.train(data=YOLO_MODEL_SEG_H_YAML,epochs = 200,batch=4 ,project="YOLO_Model_seg", name="Hyperbola_seg")
 
 
         
--- Pipeline/training_Object_Detection.py
+++ Pipeline/training_Object_Detection.py
@@ -1,7 +1,16 @@
+#Setting Working directory
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path.cwd().parent))
+
+#Import the config file so that only the Filename needs to be changed in the _read_segy function
+from config import *
+
 from ultralytics import YOLO
 
 # Load pretrained Model
 model = YOLO("yolo11n.pt")
 
 # Train on Custom dataset
-model.train(data=r"C:\vscode\PAINDGPR\Annotated_Data\YOLO_Object_Detection\data.yaml", epochs = 200, batch= 4, project="YOLO_Model_od", name="Hyperbola_od_v1")
+results = model.train(data=YOLO_MODEL_OD_YAML, epochs = 200, batch= 4, project="YOLO_Model_od", name="Hyperbola_od_v1")
